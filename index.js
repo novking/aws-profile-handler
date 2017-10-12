755b061281593186d2b6df66143f46a396cca040
Zhang <yimzhang@amazon.com>
2017-10-12 09:47:51 -0700

    refactoring to 3 class

--- index.js
+++ index.js
@@ -18,35 +18,64 @@ export class awsProfileHandler {
     getProfileCredentials(profile) {
         let credentials = this.profileObject[profile];
         if (!credentials) return null;
-        else return this._copy(credentials);
+        else return this._deepCopy(credentials);
     }
 
     deleteProfile(profile) {
-        let outputProfileObject = this._copy(this.profileObject);
+        let outputProfileObject = this._deepCopy(this.profileObject);
         delete outputProfileObject[profile];
-        this._writeToFile(outputProfileObject);
+        this._writeFile(outputProfileObject);
     }
 
     addProfile(profile, credentials) {
-        let outputProfileObject = this._copy(this.profileObject);
+        let outputProfileObject = this._deepCopy(this.profileObject);
         outputProfileObject[profile] = credentials;
-        this._writeToFile(outputProfileObject);
+        this._writeFile(outputProfileObject);
     }
 
-    _writeToFile(outputProfileObject) {
-        let encodedOutput = this._buildIniFormat(outputProfileObject);
-        fs.writeFileSync(this.filePath, encodedOutput, 'utf8');
+    _writeFile(outputProfileObject) {
+        let encodedOutput = this._encodeIniFormat(outputProfileObject);
+        fs.writeFileSync(this.filePath, encodedOutput, 'utf-8');
     }
 
-    _copy(object) {
+    _deepCopy(object) {
         return JSON.parse(JSON.stringify(object));
     }
 
     _parser(rawData) {
         return;
     }
+}
 
-    _buildIniFormat(iniObject) {
+class Ini {
+    static parse(rawData) {
+        let currentSection;
+        let map = {};
+        let lines = rawData.split(/\r?\n/);
+        lines.forEach((line) => {
+            // this regex will remove comments after
+            // each aws profile
+            line = line.split(/(^|\s)[;#]/)[0].trim();
+            if (!line) return;
+
+            let section = line.match(/^\s*\[([^\[\]]+)\]\s*$/);
+            if (section) {
+                currentSection = section[1];
+            }
+            else if (currentSection) {
+                let item = line.match(/^\s*(.+?)\s*=\s*(.+?)\s*$/);
+                if (item) {
+                    map[currentSection] = map[currentSection] || {};
+                    map[currentSection][item[1]] = item[2];
+                } else {
+                    throw new Error('Invalid ini structure');
+                }
+            }
+        });
+        return lines;
+    }
+
+    _encodeIniFormat(iniObject) {
         function _flattenObject(object) {
             let listObject = Object.keys(object).reduce((result, key) => {
                 result.push(key + "=" + object[key]);
@@ -73,4 +102,8 @@ export class awsProfileHandler {
         }, []);
         return ok;
     }
+}
+
+class Utils {
+
 }
